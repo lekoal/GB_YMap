6a89824b8fe0788846dde80b725f44172bf28b74
lekoal <coreglus@gmail.com>
2022-10-13 12:08:34 +0300

    Yandex map added

--- app/src/main/java/com/example/gbymap/MainScreenFragment.kt
+++ app/src/main/java/com/example/gbymap/MainScreenFragment.kt
@@ -1,18 +1,61 @@
 package com.example.gbymap
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.gbymap.databinding.FragmentMainScreenBinding
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.mapview.MapView
 
 class MainScreenFragment : Fragment() {
+
+    private var _binding: FragmentMainScreenBinding? = null
+    private val binding get() = _binding!!
+
+    lateinit var mapView: MapView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        MapKitFactory.setApiKey(BuildConfig.YANDEX_MAPKIT_API)
+        MapKitFactory.initialize(requireContext())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_main_screen, container, false)
+    ): View {
+        _binding = FragmentMainScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mapView = binding.mapView
+        mapView.map.move(
+            CameraPosition(
+                Point(59.939918, 30.316089),
+                16.0f,
+                0.0f,
+                0.0f
+            )
+        )
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+        MapKitFactory.getInstance().onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+        MapKitFactory.getInstance().onStop()
     }
 
     companion object {
